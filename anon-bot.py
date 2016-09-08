206e4bc7e4738a35e4874b887b550e8eeafa9611
Faeylayn <ajdobbs.sm@gmail.com>
2016-09-08 14:20:00 -0700

    updated the basics for user hashing

--- anon-bot.py
+++ anon-bot.py
@@ -1,5 +1,6 @@
 import os
 import time
+import hashlib
 from slackclient import SlackClient
 
 # starterbot's ID as an environment variable
@@ -7,15 +8,23 @@ BOT_ID = os.environ.get("BOT_ID")
 
 # constants
 ANON_CHANNEL = os.environ.get("ANON_CHANNEL")
+SALT = os.environ.get("SALT")
 
 # instantiate Slack clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
 
-def anon_message(message):
+def anon_message(message, user):
+    """
+        This function hashes the user id with the environ salt variable
+        so that the user cannot be reverse engineered and the user messages can
+        line up under the same name.
+    """
+    hashed_user = hashlib.sha1(SALT + str(user))
+    new_message = "User" + hashed_user + ": " + message
     slack_client.api_call("chat.postMessage", channel=ANON_CHANNEL,
-                          text=message)
+                          text=new_message)
 
 
 def parse_slack_output(slack_rtm_output):
@@ -30,17 +39,17 @@ def parse_slack_output(slack_rtm_output):
             if output and 'channel' in output and 'text' in output:
                 print output['channel']
                 if output['channel'][0] == "D":
-                    return output['text']
-    return None
+                    return output['text'], output['user']
+    return None, None
 
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         while True:
-            message = parse_slack_output(slack_client.rtm_read())
-            if message:
-                anon_message(message)
+            message, user = parse_slack_output(slack_client.rtm_read())
+            if message and user:
+                anon_message(message, user)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
